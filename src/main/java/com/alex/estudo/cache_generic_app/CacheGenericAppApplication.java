7e3488ba49e03ceac93b5e5bd978365701a5e962
AlexRitter1 <ritter.alex@hotmail.com>
2025-09-06 17:44:03 -0300

    initial commit

---
+++ src/main/java/com/alex/estudo/cache_generic_app/CacheGenericAppApplication.java
@@ -0,0 +1,13 @@
+package com.alex.estudo.cache_generic_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheGenericAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheGenericAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/estudo/cache_generic_app/CacheGenericAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.estudo.cache_generic_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheGenericAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
