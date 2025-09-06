545a3c145d9564841dc7512e28ed940e86300001
AlexRitter1 <ritter.alex@hotmail.com>
2025-09-06 18:34:13 -0300

    feat: add anotacao para habilitar cache no spring

--- src/main/java/com/alex/estudo/cache_generic_app/CacheGenericAppApplication.java
+++ src/main/java/com/alex/estudo/cache_generic_app/CacheGenericAppApplication.java
@@ -2,8 +2,10 @@ package com.alex.estudo.cache_generic_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CacheGenericAppApplication {
 
 	public static void main(String[] args) {
